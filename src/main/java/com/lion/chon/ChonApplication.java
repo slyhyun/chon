7e21bb9d4dd230026bef6db531df214fb7e7a4c9
slyhyun <lipsul061722@jbnu.ac.kr>
2024-07-21 18:22:55 +0900

    1.0.1

---
+++ src/main/java/com/lion/chon/ChonApplication.java
@@ -0,0 +1,13 @@
+package com.lion.chon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lion/chon/ChonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lion.chon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
