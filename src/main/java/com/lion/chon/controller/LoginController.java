dd3f99cf959f40000828f69beccd5f011ddf3d10
slyhyun <lipsul061722@jbnu.ac.kr>
2024-07-29 19:55:04 +0900

    1.0.4

---
+++ src/main/java/com/lion/chon/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.lion.chon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // login.html 템플릿을 반환
+    }
+}
